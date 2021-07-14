66d543b35165f6a7a85cf2ab23f3da153c7f51c8
Marius Arnautu <marnautu@evoraglobal.com>
2021-07-14 16:18:46 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Marius' Portfolio</title>
+  <meta name="description" content="A simple html template showcasing my projects.">
+  <meta name="author" content="Marius">
+  <meta property="og:title" content="A simple html template showcasing my projects">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="image.png">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+    <ul class='projects'>
+        <li class='padding'>
+            <h2 class='vertical-absolute'>Marius Arnautu Portfolio</h2>
+            <img src="assets/marius-arnautu.gif" alt="Marius Arnautu" style="border:1px solid" />
+        </li>
+        <li>
+            <h2>Sneakerz</h2>
+            <img src="assets/sneakerz.gif" alt="Sneakerz" />
+        </li>
+        <li>
+            <h2>Jasminescu</h2>
+            <img src="assets/jasminescu.png" alt="Jasminescu" style="border-top: 1px solid; border-bottom: 1px solid" />
+        </li>
+        <li>
+            <h2>Arnbks</h2>
+            <img src="assets/arnbks.png" alt="Arnbks" style="border-top: 1px solid #362bef; border-bottom: 1px solid #e5236e" />
+        </li>
+        <li>
+            <h2>Siera</h2>
+            <a target='_blank' href='https://xd.adobe.com/view/d58bbbd4-c6df-48b2-7413-140f76767feb-a3ba/screen/e8c795bf-b514-4831-8d28-5f0c1c1e16d8?fullscreen'>
+                <img src="assets/siera.gif" alt="Siera" style=" border-bottom: 1px solid" />
+            </a>
+        </li>
+        
+        <li>
+            <h2 style='margin-bottom: -1px'>Logos</h2>
+                <img src="assets/logos.png" alt="Logos" style="border-top: 1px solid; border-bottom: 1px solid" />
+        </li>
+    </ul>
+</body>
+</html>
